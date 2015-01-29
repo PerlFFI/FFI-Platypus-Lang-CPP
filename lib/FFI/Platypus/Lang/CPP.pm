2aac871c4ed42a7091fb878092333d762cfe03ea
Graham Ollis <plicease@cpan.org>
2015-01-29 17:08:48 -0500

    add author and copyright sections

--- lib/FFI/Platypus/Lang/CPP.pm
+++ lib/FFI/Platypus/Lang/CPP.pm
@@ -355,5 +355,16 @@ Bundle C or C++ with your FFI / Perl extension.
 
 =back
 
+=head1 AUTHOR
+
+Graham Ollis E<lt>plicease@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2015 by Graham Ollis.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
 =cut
 
