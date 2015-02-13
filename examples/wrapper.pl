57bad8b642db34673c2e4ee40e9c63230ce1f49b
Graham Ollis <perl@wdlabs.com>
2015-02-13 13:50:52 -0500

    wrapper example

---
+++ examples/wrapper.pl
@@ -0,0 +1,34 @@
+use strict;
+use warnings;
+
+package Foo;
+
+use FFI::Platypus;
+use FFI::Platypus::Memory qw( malloc free );
+
+my $ffi = FFI::Platypus->new;
+$ffi->lang('CPP');
+$ffi->lib('./wrapper.so');
+
+$ffi->custom_type( Foo => {
+  native_type => 'opaque',
+  perl_to_native => sub { ${ $_[0] } },
+  native_to_perl => sub { bless \$_[0], 'Foo' },
+});
+
+$ffi->attach( [ 'Foo_new'        => 'new'      ] => []       => 'Foo' );
+$ffi->attach( [ 'Foo_delete'     => 'DESTROY'  ] => ['Foo']  => 'void' );
+$ffi->attach( [ 'Foo::get_bar()' => 'get_bar'  ] => ['Foo']  => 'int'  );
+$ffi->attach( [ 'Foo::set_bar(int)' 
+                                 => 'set_bar'  ] => ['Foo','int']
+                                                             => 'void' );
+
+my $size = $ffi->function('Foo::_size()' => [] => 'int')->call;
+
+package main;
+
+my $foo = Foo->new;
+
+print $foo->get_bar, "\n";  # 0
+$foo->set_bar(22);
+print $foo->get_bar. "\n";  # 22
